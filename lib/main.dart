dc0b9a3f1d3f69f07e580079c2fdb26126a517d9
TheElectricBlender <yodudee97@gmail.com>
2020-02-26 22:02:44 +0800

    Used stateful widgets to increment ninja level

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class NinjaCard extends StatelessWidget {
+// NinjaCard still the widget we use
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +20,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() { //triggers build function
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
           padding: EdgeInsets.fromLTRB((30.0), 40.0, 30.0, 0.0),
@@ -55,7 +72,7 @@ class NinjaCard extends StatelessWidget {
               ),
               SizedBox(height: 10.0),
               Text(
-                '8',
+                '$ninjaLevel',
                 style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
@@ -82,3 +99,4 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
